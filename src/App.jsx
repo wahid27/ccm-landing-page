983b3e44da6fdd93ba25f657806d6a899ed76c4f
wahid27 <78307046+wahid27@users.noreply.github.com>
2026-07-06 20:05:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -141,7 +141,7 @@ export default function App() {
       { label: " Selesai", value: "100+", icon: <Target className="text-orange-500" /> },
       { label: "Sertifikasi ISO", value: "9001", icon: <ShieldCheck className="text-blue-600" /> }
     ],
-    comproUrl: "https://drive.google.com/file/d/120iriezwEXOIA3jrDmFJmPgSaHaT0z9E/view?usp=sharing",
+    comproUrl: "https://drive.google.com/file/d/1jMjU3JMrWTXHsiwhlHPsnyxXxrhzll13/view?usp=sharing",
     clients: [
       { name: "PT Bukit Asam, Tbk", icon: <Building2 className="text-orange-500" /> },
       { name: "PT PLN (Persero)", icon: <Zap className="text-blue-500" /> },
